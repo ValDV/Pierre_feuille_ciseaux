9fe5d2ed3633803d4825df1659184018bbbe991d
ValDV <valentinrsr20@gmail.com>
2023-09-14 09:34:13 +0200

    commit2

--- script.py
+++ script.py
@@ -1,24 +1,38 @@
+# Importation de la fonction 'choice' depuis le module 'random'
 from random import choice
 
-liste_signes = ["pierre","feuille", "ciseaux"]
+# Définition d'une liste contenant les options du jeu
+liste_signes = ["pierre", "feuille", "ciseaux"]
+
+# L'ordinateur choisit aléatoirement un signe parmi la liste
 signe_ordi = choice(liste_signes)
+
+# Le joueur entre son choix (pierre, feuille ou ciseaux)
 signe_joueur = input("Choisissez pierre, feuille ou ciseaux :")
+
+# Conversion du choix du joueur en minuscules pour éviter des problèmes de compréhension par la machine
 signe_joueur = signe_joueur.lower()
 
+# Affichage du choix du joueur
 print("Vous avez choisi", signe_joueur)
+
+# Affichage du choix de l'ordinateur
 print("L'Ordinateur a choisi", signe_ordi)
 
-if signe_ordi == signe_joueur :
-  print("Egalite")
-  
-elif signe_joueur == "pierre" and signe_ordi == "ciseaux" :
-  print("Victoire Utilisateur")
-  
-elif signe_joueur == "feuille" and signe_ordi == "pierre" :
-  print("Victoire Utilisateur")
-  
-elif signe_joueur == "ciseaux" and signe_ordi == "feuille" :
-  print("Victoire Utilisateur")
-  
-else :
-  print("Victoire Ordinateur")
+# Si les deux choix sont identiques, il y a égalité
+if signe_ordi == signe_joueur:
+    print("Egalite")
+
+# Si une de ces conditions se réalise, le joueur gagne
+elif signe_joueur == "pierre" and signe_ordi == "ciseaux":
+    print("Victoire Joueur")
+
+elif signe_joueur == "feuille" and signe_ordi == "pierre":
+    print("Victoire Joueur")
+
+elif signe_joueur == "ciseaux" and signe_ordi == "feuille":
+    print("Victoire Joueur")
+
+# Si aucune des conditions précédentes se réalise, c'est l'ordinateur qui gagne
+else:
+    print("Victoire Ordinateur")
